007f2db3a201553b6660c9f84d705975d328abc4
Ivan Markovski <sagitariusim@gmail.com>
2021-11-11 11:14:34 +0100

    11/11/2021 11:14

--- DatabaseContext/Vlsmresult.cs
+++ DatabaseContext/Vlsmresult.cs
@@ -15,18 +15,25 @@ namespace DatabaseContext
         [Column("id")]
         [Key]
         public long Id { get; set; }
+
         [Column("NetworkID", TypeName = "VARCHAR (45)")]
         public string NetworkId { get; set; }
+
         [Column("CIDR", TypeName = "VARCHAR (45)")]
         public string Cidr { get; set; }
+
         [Column(TypeName = "VARCHAR (45)")]
         public string SubnetMask { get; set; }
+
         [Column(TypeName = "VARCHAR (45)")]
         public string NumberOfHostsPerSubnet { get; set; }
+
         [Column(TypeName = "VARCHAR (45)")]
         public string NumberOfSubnets { get; set; }
+
         [Column("RangeOfUsableIPaddresses", TypeName = "VARCHAR (45)")]
         public string RangeOfUsableIpaddresses { get; set; }
+
         [Column("broadcastID", TypeName = "VARCHAR (45)")]
         public string BroadcastId { get; set; }
     }
